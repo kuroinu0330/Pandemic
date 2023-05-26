bdd8f0768555e1e1e9177607c158edfcfb89f2b8
niikidaisuke <TTC22D3170012@edu.tech.ac.jp>
2023-05-26 10:18:34 +0900

    リザルト画面　スコアを表示　a版　

---
+++ Assets/Menber/niikidaisuke/Scripts/Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text ScoreText;
+    int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = ScoreData.getscore();
+
+        ScoreText.text = string.Format("{0}", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
