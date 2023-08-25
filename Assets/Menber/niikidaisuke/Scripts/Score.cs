9b5616c55a19faf344b0ae115c3756acae849600
niikidaisuke <>
2023-08-25 17:41:05 +0900

    リザルト画面のコメの挙動とそれを受け止めるコップのようなものを作りました。

--- Assets/Menber/niikidaisuke/Scripts/Score.cs
+++ Assets/Menber/niikidaisuke/Scripts/Score.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text ScoreText;
-    int score;
+    public static int score;
     float timeLine;
     // Start is called before the first frame update
     void Start()
